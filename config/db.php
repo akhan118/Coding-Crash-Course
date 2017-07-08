15104f2b9a0dca7c4438072f9253f1930a6d6bf3
Nidra <deesmity7@yahoo.com>
2017-07-08 14:26:40 -0400

    root password updated

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=project2',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
