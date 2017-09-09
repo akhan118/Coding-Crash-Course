cadf3c642b8ac831198d3472ad6c5e7054782c2b
Ahmad Khan <ahmad.khan@cusolutionsgroup.com>
2017-09-09 12:44:30 -0600

    comment out old code
    comment out old code

--- models/CreateProductForm.php
+++ models/CreateProductForm.php
@@ -30,35 +30,29 @@ class CreateProductForm extends Model
         ];
     }
 
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
-     *
-     * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
-     */
-    public function validatePassword($attribute, $params)
-    {
-        if (!$this->hasErrors()) {
-            $user = $this->getUser();
-
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
-            }
-        }
-    }
-
-    /**
-     * Logs in a user using the provided username and password.
-     * @return bool whether the user is logged in successfully
-     */
-    public function login()
-    {
-        if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
-        }
-        return false;
-    }
+    // 
+    // public function validateForm($attribute, $params)
+    // {
+    //     if (!$this->hasErrors()) {
+    //         $user = $this->getUser();
+    //
+    //         if (!$user || !$user->validatePassword($this->password)) {
+    //             $this->addError($attribute, 'Incorrect username or password.');
+    //         }
+    //     }
+    // }
+    //
+    // /**
+    //  * Logs in a user using the provided username and password.
+    //  * @return bool whether the user is logged in successfully
+    //  */
+    // public function login()
+    // {
+    //     if ($this->validate()) {
+    //         return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+    //     }
+    //     return false;
+    // }
 
 
 
